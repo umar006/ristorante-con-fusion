01ee3bfacfa9105191ae1f5e1b94c9ffc5a967b2
umar006 <umaralfaruq597@gmail.com>
2020-11-16 21:05:23 +0700

    add automation script

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+   $("#mycarousel").carousel({ interval: 2000, pause: false });
+   $("#carousel-button").click(function () {
+      if ($("#carousel-button").children("span").hasClass("fa-pause")) {
+         $("#mycarousel").carousel("pause");
+         $("#carousel-button").children("span").removeClass("fa-pause").addClass("fa-play");
+      } else {
+         $("#mycarousel").carousel("cycle");
+         $("#carousel-button").children("span").removeClass("fa-play").addClass("fa-pause");
+      }
+   });
+
+   // Reserve table modal
+   $("#btn-reserve").click(function () {
+      $('#reserve-modal').modal('show');
+   });
+   $('#cancel-reserve').click(function () {
+      $('#reserve-modal').modal('hide');
+   });
+
+   // Login modal
+   $('#btn-login').click(function () {
+      $('#login-modal').modal('show');
+   });
+   $('#cancel-login').click(function () {
+      $('#login-modal').modal('hide');
+   });
+});
